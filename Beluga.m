4c6f8beae501ab5cc0b5e3a0f2d8a1777839e364
Brendan Andrade <brnd4n@gmail.com>
2013-07-09 20:07:42 -0400

    Adding empty class file for Beluga API

---
+++ Beluga.m
@@ -0,0 +1,12 @@
+classdef Beluga < dcsl_robot
+    %UNTITLED4 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
