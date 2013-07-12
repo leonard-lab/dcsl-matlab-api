ac5eda66d09c58292e49e71a961e0a51e1f29fb6
Brendan Andrade <bandrade@princeton.edu>
2013-07-12 12:26:06 -0400

    Added cells and removed pause to adjust for new active runtime feature of Miabots

--- TestScriptRun2.m
+++ TestScriptRun2.m
@@ -1,10 +1,12 @@
 clear m
-initial_poses = [-0.5 0 0 0; 0 0 0 0];
+initial_poses = [0.5 0 0 0; -0.2 0.3 0 pi/2];
 runtime = 10;
 noise = [0.01, 0.01, 0, 0.01];
 m = Miabots(initial_poses, @TestControlLaw, 'velocity', runtime, 'sim', false);
+
+%%
 m.start
-pause(runtime);
+%%
 m.shutdown
 %%
 plot(m.get_history(1,'x'), m.get_history(1,'y'), m.get_history(2,'x'), m.get_history(2,'y'));
