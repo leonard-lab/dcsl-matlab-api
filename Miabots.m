52b844e584e8d75400caf6298ad04da7b11997bc
Brendan Andrade <bandrade@princeton.edu>
2013-07-13 12:48:34 -0400

    Added documentation to Miabots class.

--- Miabots.m
+++ Miabots.m
@@ -5,6 +5,9 @@ classdef Miabots < dcsl_robot
     % a control law, a control method, and a run time duration. The class
     % can be configured to simulate the system.
     %
+    % Summary provided below. More help can be found by typing doc Miabots
+    % or help Miabots.method_name.
+    %
     % The connection to the ROS system requires installation of the
     % web-matlab-bridge available at
     % https://github.com/BrendanAndrade/web-matlab-bridge.
@@ -17,36 +20,47 @@ classdef Miabots < dcsl_robot
     % initial_poses: n_robots X [x y z theta] matrix containing the initial
     % positions and headings of the robots.
     %
-    % control_law:  Function handle to user provided control law.
-    % Function should accept time and current states of the robots and
-    % return input commands for the robots. Time should be a scalar in
-    % seconds. Current states should be a n_robots X 7 matrix with the
-    % second dimension in the format [x y z vx vz theta theta_dot].
-    % Commands should be returned as a n_robots X 3 matrix with the
-    % second dimension in the format [u_x u_theta u_z].
-    % Example: @control_law if control takes two arguments (time, states)
-    % Example: @(t,x) control_law(t,x,additional,arguments)
+    % control_law:  Function handle to user provided control law. Function
+    % should accept time and current states of the robots and return input
+    % commands for the robots. Time should be a scalar in seconds. Current
+    % states should be a n_robots X 7 matrix with the second dimension in
+    % the format [x y z vx vz theta theta_dot]. If in velocity (or direct)
+    % control mode, commands should be returned as a n_robots X 3 matrix
+    % with the second dimension in the format [u_x u_theta u_z]. If in
+    % waypoint mode, commands should be returned as a n_robots X 4 matrix
+    % with the second dimension [x y z theta] of the goal pose. Example:
+    % @control_law if control takes two arguments (time, states) Example:
+    % @(t,x) control_law(t,x,additional,arguments)
     %
     % control_mode: 'velocity' 'waypoint' or 'direct'
     %
     % run_time: Time in seconds to run the system or simulation. Provide
-    % Inf to run system indefinitely.
+    % Inf to run system indefinitely (run mode only).
     %
     % OPTIONS
     % 'sim': Logical. Default: false. true to simulate dynamics in MATLAB.
     % false to run ROS.
     %
     % 'sim_noise': Length 4 Vector. Default: [0 0 0 0]. Standard deviation
-    % of the random gaussian noise applied to [x y z theta] estimates during
-    % simulation.
+    % of the random gaussian noise applied to [x y z theta] measuremente
+    % estimates during simulation.
     %
-    % 'Ts': Number. Default: 0.04. Time step for measurement/control
-    % update. Only affects simulation.
+    % 'Ts': Number. Default: 0.0667. Time step for measurement/control
+    % update. Only affects simulation. If your control law relies on the
+    % time step, it is best to calculate it using the t input to the
+    % control law.
     %
     % 'URI': String. Default: 'ws://localhost:9090'. URI of rosbridge
     % server.
     %
     % PROPERTIES
+    %
+    %   wp_k1 - Control constant for velocity feedback in waypoint
+    %   controller
+    %
+    %   wp_k2 - Control constant for angular feedback in waypoint
+    %   controller
+    %
     %   n_robots - Number of robots to use. Determined by size of
     %   initial_poses given at initialization of object.
     % 
@@ -61,7 +75,7 @@ classdef Miabots < dcsl_robot
     %   third dimension is [time u_x u_theta u_z].
     %
     %   control_mode - 'velocity', 'waypoint', or 'direct' control of the
-    %   robots. Set at initialization of object.
+    %   robots. Set at initialization of object or via set method.
     %
     %   control_law - Function handle to user provided control law.
     %   Function should accept time and current states of the robots and
@@ -79,6 +93,10 @@ classdef Miabots < dcsl_robot
     %   system in MATLAB. False if system should command ROS. Set via
     %   set_sim or initialization with 'sim' option.
     %
+    %   sim_noise -  Length 4 Vector. Default: [0 0 0 0]. Standard
+    %   deviation of the random gaussian noise applied to [x y z theta]
+    %   measuremente estimates during simulation.
+    %
     %   Ts - Default: 0.04 (25 Hz). Applies only to
     %   simulation. Time step to update control loop/receive updated state.
     %   Set via set.Ts or initialization with 'Ts' option.   
@@ -91,17 +109,30 @@ classdef Miabots < dcsl_robot
     %   connect - Setup connect to ROS without starting control.
     %
     %   start - Begin simulation or connect if necessary and start control
-    %   of ROS system.
+    %   of ROS system. Robots will move to initial poses and then system
+    %   will execute the control law for the run time.
     %
     %   stop - Stop robots in ROS. Interrupt timed run or end
-    %   indefinite run. Does not effect MATLAB simulation.
+    %   indefinite run. Does not effect MATLAB simulation. Sets inputs to
+    %   zero.
     %
     %   shutdown - Stop robots and close connection to ROS.
     %   
     %   command - Send command to ROS system if it is not beinging
-    %   automatically controlled. Command should be in the format: n_robots
-    %   X 4 matrix with the second dimension in the format [u_x u_theta
-    %   u_z].
+    %   automatically controlled. If in velocity or direct mode, command
+    %   should be in the format: n_robots X 3 matrix with the second
+    %   dimension in the format [u_x u_theta u_z]. If waypoint mode,
+    %   n_robots X 4 with second dimension [x y z theta] as the goal
+    %   waypoint.
+    %
+    %   enable_control - turns on control law manually and begins gathering
+    %   state infomration
+    %
+    %   disable_control - turns off control law, system will still poll
+    %   state information
+    %
+    %   go_to_poses - moves the robots to supplied poses. Works in sim and
+    %   run modes.
     %
     %   get_history(robot_ID, parameter) robot_ID is the index of the
     %   robot's initial_poses in that matrix. parameter options are:
@@ -112,11 +143,12 @@ classdef Miabots < dcsl_robot
     %   'x', 'y', 'z', 'vx', 'vz', 'theta', 'theta_dot': returns
     %   n_time_steps vector with corresponding state history
     %   'command': returns n_time_stepsX3 array with second dimension as
-    %   [ux utheta uz]
+    %   [ux utheta uz] or [x y z theta]
     %   'command_times': returns n_time_steps vector with times of command
     %   sends
-    %   'ux' 'utheta' 'uz': returns n_time_steps vector with corresponding
-    %   command history
+    %   'u1' 'u2' 'u3', 'u4': returns n_time_steps vector with corresponding
+    %   command history. If velocity or direct u1=ux u2=utheta u3=uz. If
+    %   waypoint u1=x u2=y u3=z u4=theta of the waypoints.
     %
     % LICENSE
     %
@@ -168,6 +200,14 @@ classdef Miabots < dcsl_robot
         % ROS related methods
                
         function ros_stop(obj)
+            % ROS_STOP Stops the robots' movement through the ROS system.
+            %
+            % SYNOPSIS ros_stop(obj)
+            %
+            % INPUT obj: the object
+            %
+            % OUTPUT none
+            
             obj.control_on = false;
             % Wait for callbacks to complete
             drawnow();
@@ -177,13 +217,34 @@ classdef Miabots < dcsl_robot
         end
         
         function [direct_pub] = setup_direct_pub(obj, ros_websocket)
+            % SETUP_DIRECT_PUB sets up the direct mode ROS publisher for
+            % the miabot robots
             %
+            % SYNOPSIS [direct_pub] = setup_direct_pub(obj, ros_websocket)
+            %
+            % INPUT obj: the object
+            % ros_websocket: a ros_websocket object from the
+            % web-matlab-bridge to which to publish
+            %
+            % OUTPUT direct_pub: the publisher object for publishing direct
+            % inputs to the miabot driver.
             
             direct_pub = Publisher(ros_websocket, 'velocity_input', 'dcsl_messages/TwistArray');
         end
         
         function [commands_struct] = commands_mat2dir_struct(obj, commands_mat)
+            % COMMANDS_MAT2DIR_STRUCT Convert the direct commands matrix to
+            % a struct formatted in the TwistArray ROS message.
             %
+            % SYNOPSIS [commands_struct] = commands_mat2dir_struct(obj, commands_mat)
+            %
+            % INPUTS obj: the object
+            % commands_mat: an n_robots X 3 matrix where the second
+            % dimension is formatted as [ux utheta uz]
+            %
+            % OUTPUT commands_struct: a struct containing the given
+            % commands formatted as a ROS TwistArray message as defined in
+            % the dcsl_messages package.
             
             commands_struct = obj.commands_mat2vel_struct(commands_mat);
         end
@@ -191,6 +252,24 @@ classdef Miabots < dcsl_robot
         % Simulation related methods
         
         function [states_out, measurements_out] = propagate(obj, states_in, commands_in, dt, noise)
+            % PROPAGATE Takes in current state, commands, time duration,
+            % and measurement noises and propagates kinematics forward and
+            % returns states and states with measurement noise.
+            %
+            % SYNOPSIS [states_out, measurements_out] = propagate(obj, states_in, commands_in, dt, noise)
+            %
+            % INPUTS obj: the object
+            % states_in: an n_robots X 7 matrix with the second dimension
+            % as the state of the robot [x y z vx vz theta theta_dot]
+            % commands_in: an n_robots X M inputs matrix. If velocity or
+            % direct control format of 2nd dimension is [ux utheta uz]; if
+            % it is waypoint control the format is [x y z theta] of the
+            % waypoint
+            % dt: the time to propagate the system in seconds
+            % noise: a length 4 vector containing the standard deviation of
+            % the guassian noise to be applied to the measurement in the
+            % format [x y z theta].
+            
             states_out = zeros(obj.n_robots, 7);
             measurements_out = zeros(obj.n_robots, 7);
             for i=1:obj.n_robots
@@ -253,7 +332,21 @@ classdef Miabots < dcsl_robot
         end
         
         function [ux, utheta, uz] = wp_law(obj, state, waypoint)
+            % WP_LAW The waypoint controller. Returns the direct inputs
+            % given the state and desired waypoint for an INDIVIDUAL robot.
             %
+            % SYNOPSIS [ux, utheta, uz] = wp_law(obj, state, waypoint)
+            %
+            % INPUTS obj: the object
+            % state: a length 7 vector in the format [x y z vx vz theta
+            % theta_dot] containing the current state of the robot.
+            % waypoint: a lenght 4 vector in the format [x y z theta]
+            % containing the pose of the waypoint
+            %
+            % OUTPUT: ux - the x velocity input
+            % utheta - the angular velocity input
+            % uz - the z velocity input
+            
             
             % Find phi, the angle from the heading of the robot to the
             % heading from the robot to the waypoint.
