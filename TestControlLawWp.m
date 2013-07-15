878a215004e3a696f5c60ad8becabae5df555cef
Brendan Andrade <bandrade@princeton.edu>
2013-07-15 10:28:00 -0400

    Updated test waypoint files with reachable goal poses. Added cells.

--- TestControlLawWp.m
+++ TestControlLawWp.m
@@ -5,11 +5,12 @@ function [ commands ] = TestControlLawWp(t, states )
 n_robots = size(states, 1);
 
 commands  = zeros(n_robots, 4);
-commands(1,1) = .8;
+commands(1,1) = .5;
 commands(1,2) = .2;
+commands(1,4) = -pi/2;
 
-commands(2,1) = -.6;
-commands(2,2) = -.7;
-
+commands(2,1) = -.2;
+commands(2,2) = -.3;
+commands(2,4) = pi/2;
 end
 
