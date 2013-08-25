439dff0cd4784294fd92628a79ea27033c41bb17
Brendan Andrade <brnd4n@gmail.com>
2013-08-24 22:09:02 -0400

    Added cell structure to beluga test script

--- TestScriptBelugas.m
+++ TestScriptBelugas.m
@@ -1,10 +1,12 @@
 clear m
 close all
-initial_poses = [0 0 0 0];
+initial_poses = [0 0 0 0; 0 0 0 0];
 runtime = 5;
 noise = [0.01, 0.01, 0, 0.01];
-m = Belugas(initial_poses, @TestControlLaw, 'direct', runtime, 'sim', true);%, 'sim_noise', noise);
+m = Belugas(initial_poses, @TestControlLaw, 'direct', runtime, 'sim', false);%, 'sim_noise', noise);
 m.start
+
+%%
 plot(m.get_history(1,'x'), m.get_history(1,'y'))%,m.get_history(2,'x'),m.get_history(2,'y'));
 title('x-y')
 axis('equal');
