cd18bde672ce9dd36a73a9dd693ce1c2eb3e2213
Brendan Andrade <bandrade@princeton.edu>
2013-09-07 14:27:48 -0400

    Updated Test beluga script for live running

--- TestScriptBelugas.m
+++ TestScriptBelugas.m
@@ -4,13 +4,11 @@ initial_poses = [0 0 0 0; 0 0 0 0];
 runtime = 5;
 noise = [0.01, 0.01, 0, 0.01];
 m = Belugas(initial_poses, @TestControlLaw, 'direct', runtime, 'sim', false);%, 'sim_noise', noise);
-m.start
+m.start('use_initial_poses', false)
 
 %%
+m.shutdown();
 plot(m.get_history(1,'x'), m.get_history(1,'y'))%,m.get_history(2,'x'),m.get_history(2,'y'));
 title('x-y')
 axis('equal');
-figure;
-plot(m.get_history(1,'state_times'), m.get_history(1,'z')); title('z');
-figure;
-plot(m.get_history(1,'state_times'), m.get_history(1,'x')); title('x');
+m.plot_history(1);
